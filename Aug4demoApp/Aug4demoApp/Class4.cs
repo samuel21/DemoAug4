6de010f4add556a736c9ed089303e411a8fff242
sajoy <sajoy@SAJOY>
2017-08-04 17:10:02 +0530

    new file

---
+++ Aug4demoApp/Aug4demoApp/Class4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug4demoApp
+{
+    class Class4
+    {
+    }
+}
